8949010cfb90d4cac652cc4a6d043a6bb6eaba5a
Odile078 <ouwimpuhwe620@daviscollege.com>
2021-03-11 00:15:15 +0200

    Adding the footer

---
+++ src/images/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Services from './components/Services'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Services />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
